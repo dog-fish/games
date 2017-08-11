0eab8c3cd7a8def7fc44d182344283123d31c350
Xiaohao Zhou <staylqzz@163.com>
2017-08-11 12:07:53 +0800

    第一次提交

---
+++ block.js
@@ -0,0 +1,31 @@
+var Block = function(game, position) {
+    // positon 是 [0, 0] 格式
+    // 讲每个图片都载入，在position中写入不同的图片。不同的关卡画不同的图片。
+    var p = position
+    var img = game.imageByName('block')
+    var o = {
+        x: p[0],
+        y: p[1],
+        alive: false,
+        lifes: p[2] || 1,
+    }
+    o.image = img.image
+    o.w = img.w
+    o.h = img.h
+    o.kill = function() {
+        o.lifes--
+        if (o.lifes < 1) {
+            o.alive = false
+        }
+    }
+    o.collide = function(b) {
+        // log('block', o.alive, b)
+        return o.alive && (rectIntersects(o, b) || rectIntersects(b, o))
+    }
+    o.hasPoint = function(x, y) {
+        var xIn = x >= o.x && x <= o.x + o.w
+        var yIn = y >= o.y && y <= o.y + o.h
+        return xIn && yIn
+    }
+    return o
+}
